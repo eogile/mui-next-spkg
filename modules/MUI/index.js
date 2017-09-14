e0e30fb11dc7639fa08d01c1a466b47050a98143
Jean Blanchard <jblanchard@eogile.com>
2017-09-14 18:01:40 +0200

    Add a bunch of components

--- modules/MUI/index.js
+++ modules/MUI/index.js
@@ -4,13 +4,17 @@ import LayoutItem from './components/LayoutItem';
 import AppBar from 'material-ui/AppBar';
 import Avatar from 'material-ui/Avatar';
 import Button from 'material-ui/Button';
+import Card, { CardActions, CardContent, CardMedia, CardHeader } from 'material-ui/Card';
+import Chip from 'material-ui/Chip';
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from 'material-ui/Dialog';
+import Divider from 'material-ui/Divider';
+import Drawer from 'material-ui/Drawer';
 import IconButton from 'material-ui/IconButton';
-import { MenuItem } from 'material-ui/Menu';
-import Paper from 'material-ui/Paper';
-import Select from './components/Select';
-import TextField from 'material-ui/TextField';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
 import List, {
   ListItem,
   ListItemAvatar,
@@ -18,6 +22,16 @@ import List, {
   ListItemSecondaryAction,
   ListItemText,
 } from 'material-ui/List';
+import { MenuItem } from 'material-ui/Menu';
+import Paper from 'material-ui/Paper';
+import { CircularProgress, LinearProgress } from 'material-ui/Progress';
+import Radio, { RadioGroup } from 'material-ui/Radio';
+import Select from './components/Select';
+import Table, { TableBody, TableCell, TableHead, TableRow, TableSortLabel } from 'material-ui/Table';
+import TextField from 'material-ui/TextField';
+import Toolbar from 'material-ui/Toolbar';
+import Tooltip from 'material-ui/Tooltip';
+import Typography from 'material-ui/Typography';
 import './css/styles.css';
 
 export {
@@ -27,6 +41,19 @@ export {
   AppBar,
   Avatar,
   Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CardHeader,
+  Chip,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Divider,
+  Drawer,
   IconButton,
   List,
   ListItem,
@@ -36,8 +63,19 @@ export {
   ListItemText,
   MenuItem,
   Paper,
+  CircularProgress,
+  LinearProgress,
+  Radio,
+  RadioGroup,
   Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableSortLabel,
   TextField,
   Toolbar,
+  Tooltip,
   Typography,
 };
